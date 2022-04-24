a3dc3b12d2ce0e45e251bcdf316fbd6ca27ba6d8
Nicu Taban <nictbndeveloper@gmail.com>
2022-04-24 16:45:53 +0300

    Brings Hibernate Code

---
+++ src/test/java/guru/springframework/jdbc/BookDaoHibernateTest.java
@@ -0,0 +1,111 @@
+package guru.springframework.jdbc;
+
+import guru.springframework.jdbc.dao.BookDao;
+import guru.springframework.jdbc.dao.BookDaoHibernate;
+import guru.springframework.jdbc.domain.Book;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.ActiveProfiles;
+
+import javax.persistence.EntityManagerFactory;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("local")
+@DataJpaTest
+@ComponentScan(basePackages = {"guru.springframework.jdbc.dao"})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class BookDaoHibernateTest {
+    @Autowired
+    EntityManagerFactory emf;
+
+    BookDao bookDao;
+
+    @BeforeEach
+    void setUp() {
+        bookDao = new BookDaoHibernate(emf);
+    }
+
+    @Test
+    void findAllBooksSortByTitle() {
+    }
+
+    @Disabled
+    @Test
+    void findAllBooks() {
+        List<Book> books = bookDao.findAllBooks(PageRequest.of(0, 10));
+        assertThat(books).isNotNull();
+        assertThat(books.size()).isEqualTo(10);
+    }
+
+    @Test
+    void testFindAllBooks() {
+    }
+
+    @Test
+    void getById() {
+        Book book = bookDao.getById(3L);
+
+        assertThat(book.getId()).isNotNull();
+    }
+
+    @Test
+    void findBookByTitle() {
+        Book book = bookDao.findBookByTitle("Clean Code");
+        assertThat(book).isNotNull();
+    }
+
+    @Test
+    void saveNewBook() {
+        Book book = new Book();
+        book.setIsbn("1234");
+        book.setPublisher("Self");
+        book.setTitle("my book");
+
+        book.setAuthorId(1L);
+        Book saved = bookDao.saveNewBook(book);
+
+        assertThat(saved).isNotNull();
+    }
+
+    @Test
+    void updateBook() {
+        Book book = new Book();
+        book.setIsbn("1234");
+        book.setPublisher("Self");
+        book.setTitle("my book");
+
+
+        book.setAuthorId(1L);
+        Book saved = bookDao.saveNewBook(book);
+
+        saved.setTitle("New Book");
+        bookDao.updateBook(saved);
+
+        Book fetched = bookDao.getById(saved.getId());
+
+        assertThat(fetched.getTitle()).isEqualTo("New Book");
+    }
+
+    @Test
+    void deleteBookById() {
+        Book book = new Book();
+        book.setIsbn("1234");
+        book.setPublisher("Self");
+        book.setTitle("my book");
+        Book saved = bookDao.saveNewBook(book);
+
+        bookDao.deleteBookById(saved.getId());
+
+        Book deleted = bookDao.getById(saved.getId());
+
+        assertThat(deleted).isNull();
+    }
+}
